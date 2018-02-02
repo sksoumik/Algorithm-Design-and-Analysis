80d89ea744a6c2024980ef913237ac0d3e170452
Sadman Kabir Soumik <sadmanks@gmail.com>
2018-02-03 01:46:28 +0600

    Fibonacchi Series recursion

---
+++ Recursion/FibonacchiSeries.py
@@ -0,0 +1,14 @@
+# Fibonacci series using recursion
+def fibonacci(n):
+    if n == 0:
+        return n
+    elif n == 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+n = int(input("Enter how many numbers you want to print:"))
+print("Fibonacci sequence:")
+for i in range(n):
+    print(fibonacci(i), end='  ')
